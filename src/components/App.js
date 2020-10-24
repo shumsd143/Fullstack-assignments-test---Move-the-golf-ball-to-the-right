8e13af782ded5d87d59abfe06e75b4550af8aedf
Shubham Shekhar <shubhamshekhar123.ss@gmail.com>
2020-10-25 00:41:30 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -5,40 +5,39 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            renderBall: false,
+            renderBall: false,//variable needed to be changed
             posi : 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderChoice.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.ArrowRight = this.ArrowRight.bind(this)
     };
 
-
+    //call back function
     buttonClickHandler() {
-        this.setState({ renderBall: true })
+        this.setState({
+            renderBall:true
+        })
     }
-    renderChoice() {
-        if (this.state.renderBall === false) {
-            return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-        }
-        else if (this.state.renderBall === true) {
-            return <div className="ball" style={this.state.ballPosition}></div>
+    ArrowRight(e) {
+        if (e.key === 'ArrowRight') {
+            this.setState({
+                 ballPosition: { left: this.state.posi+ "px"}, posi: this.state.posi + 5 
+            })
         }
     }
 
+    //bind ArrowRight keydown event
     componentDidMount() {
-        
-        document.addEventListener("keydown", (e) => {
-            if (e.key === 'ArrowRight') {
-                this.setState({ ballPosition: { left: this.state.posi+ "px"}, posi: this.state.posi + 5 })
-            }
+        document.addEventListener('keydown',(e)=>{
+            this.ArrowRight(e)
         })
     }
 
     render() {
         return (
             <div className="playground">
-                {this.renderChoice()}
+                {this.state.renderBall?<div className="ball" style={this.state.ballPosition}></div>:<button onClick={this.buttonClickHandler} >Click For One Ball</button>}
             </div>
         )
     }
